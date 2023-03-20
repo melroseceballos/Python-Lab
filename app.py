d64babadf4b6ae5a61799afadd28d3e6357dbdf5
Scott Draper <scott.draper@generalassemb.ly>
2023-03-20 15:41:40 -0600

    Added solutions

--- app.py
+++ app.py
@@ -11,6 +11,12 @@
 # Hints:  Use the in operator to check if a character is in another string
 #         For example, if some_char in 'abc':
 # ------------------------------------------------------------------ #
+letter = input(
+    'Please enter a letter from the alphabet (a-z or A-Z): ').lower()
+if letter in 'a e i o u y':
+    print(f'The letter {letter} is a vowel')
+else:
+    print(f'The letter {letter} is a consonant')
 
 
 # Exercise 2: Length of Phrase
@@ -22,6 +28,10 @@
 #      - What you entered is xx characters long
 # 3. Return to step 1, unless the word 'quit' was entered.
 # ------------------------------------------------------------------ #
+phrase = ''
+while phrase != 'quit':
+    phrase = input('Please enter a word or phrase: ')
+    print(f'What you entered is {len(phrase)} characters long')
 
 
 # Exercise 3: Calculate Dog Years
@@ -39,6 +49,12 @@
 # Use the int() function to convert the string returned from input() into an integer
 # Start with an if that checks if the age is less than 3
 # ------------------------------------------------------------------ #
+human_years = int(input("Input a dog's age in human years: "))
+if human_years < 3:
+    dog_years = human_years * 10
+else:
+    dog_years = 20 + (human_years - 2) * 7
+print(f"The dog's age in dog years is {dog_years}")
 
 
 # Exercise 4: What kind of Triangle?
@@ -56,6 +72,18 @@
 # 3. Print a message such as:
 #      - A triangle with sides of <a>, <b> & <c> is a <type of triangle> triangle
 # ------------------------------------------------------------------ #
+print('Enter the lengths of three side of a triangle:')
+a = input('a: ')
+b = input('b: ')
+c = input('c: ')
+
+if a == b and b == c:
+    print(
+        f'A triangle with sides of {a}, {b} & {c} is an equalateral triangle')
+elif a != b and a != c and b != c:
+    print(f'A triangle with sides of {a}, {b} & {c} is a scalene triangle')
+else:
+    print(f'A triangle with sides of {a}, {b} & {c} is an isosceles triangle')
 
 
 # Exercise 5: Fibonacci sequence for first 50 terms
@@ -76,6 +104,31 @@
 # Use a while loop with a looping variable, or look into Python ranges, e.g.:
 #   for n in range(50):
 # ------------------------------------------------------------------ #
+# Using a range with a for loop
+a = 1
+b = 1
+for term in range(50):
+    if term < 2:
+        print('term: {term} / number: {term}')
+    else:
+        c = a + b
+        print(f'term: {term} / number: {c}')
+        a = b
+        b = c
+
+# Using a while loop
+term = 0
+a = 0
+b = 1
+while term < 50:
+    if term < 2:
+        print(f'term: {term} / number: {term}')
+    else:
+        num = a + b
+        print(f'term: {term} / number: {num}')
+        a = b
+        b = num
+    term += 1
 
 
 # Exericse 6: What's the Season?
@@ -100,3 +153,35 @@
 # After setting the likely season, you can use another if...elif...else statement to "adjust" if
 # the day number falls within a certain range.
 # ------------------------------------------------------------------ #
+mo = input('Enter the month of the season (Jan - Dec): ')
+day = int(input('Enter the day of the month: '))
+if mo in ('Jan', 'Feb', 'Mar'):
+    season = 'Winter'
+elif mo in ('Apr', 'May', 'Jun'):
+    season = 'Spring'
+elif mo in ('Jul', 'Aug', 'Sep'):
+    season = 'Summer'
+else:
+    season = 'Fall'
+if mo == 'Mar' and day > 19:
+    season = 'Spring'
+elif mo == 'Jun' and day > 20:
+    season = 'Summer'
+elif mo == 'Sep' and day > 21:
+    season = 'Fall'
+elif mo == 'Dec' and day > 20:
+    season = 'Winter'
+print(f'{mo} {day} is in {season}')
+
+# More concise using the Python version of a ternary expression...
+mo = input('Enter the month of the year (Jan - Dec): ')
+day = int(input('Enter the day of the month: '))
+if mo in ('Dec', 'Jan', 'Feb'):
+    season = 'Fall' if mo == 'Dec' and day < 21 else 'Winter'
+elif mo in ('Mar', 'Apr', 'May'):
+    season = 'Winter' if mo == 'Mar' and day < 20 else 'Spring'
+elif mo in ('Jun', 'Jul', 'Aug'):
+    season = 'Spring' if mo == 'Jun' and day < 21 else 'Summer'
+else:
+    season = 'Summer' if mo == 'Sep' and day < 22 else 'Fall'
+print(f'{mo} {day} is in {season}')
